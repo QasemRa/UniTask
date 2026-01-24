267373632dd4de34b65d461eb8e5dc2d17e8ed27
QasemRa <kkaasm551@gmail.com>
2026-01-24 22:44:36 +0300

    Fix static files design with whitenoise

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,14 +76,9 @@ USE_TZ = True
 # =========================
 # STATIC FILES (CSS/JS)
 # =========================
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
-
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
