a6d41a8866932a1bdca6c474e7f96429e7ea4f08
QasemRa <kkaasm551@gmail.com>
2026-01-24 22:24:21 +0300

    Add auto superuser script

--- createsuperuser_render.py
+++ createsuperuser_render.py
@@ -2,12 +2,21 @@ import os
 import django
 from django.contrib.auth import get_user_model
 
+# تأكد إن 'mysite' هو اسم الفولدر اللي بيه ملف settings.py
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 django.setup()
 
 User = get_user_model()
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'YourPassword123')
-    print("Superuser created!")
+username = '1'
+password = '1' # هذا الرمز مالتك، تكدر تغيره
+
+user, created = User.objects.get_or_create(username=username)
+user.set_password(password) # هاي الخطوة تضمن إن الرمز يصير YourPassword123 حتى لو الحساب قديم
+user.is_superuser = True
+user.is_staff = True
+user.save()
+
+if created:
+    print("Superuser created successfully!")
 else:
-    print("Superuser already exists.")
+    print("Superuser password updated successfully!")
